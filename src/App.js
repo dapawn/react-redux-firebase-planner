7b3da358ddaf34b765a9652ce5ceeb26449334b2
dapawn <dapawn@gmail.com>
2019-09-13 18:08:49 -0400

    Added Some Sytle.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import CreateProject from './components/projects/CreateProject';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 
@@ -13,6 +14,7 @@ function App() {
 	<Navbar />
         <Switch>
           <Route exact path='/' component={Dashboard} />
+          <Route exact path='/project/new' component={CreateProject} />
           <Route path='/project/:id' component={ProjectDetails} />
           <Route path='/signin' component={SignIn} />
           <Route path='/signup' component={SignUp} />
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const SignedInLinks = () => {
   return (
       <ul className="right">
-        <li><NavLink to='/'>New Project</NavLink></li>
+        <li><NavLink to='/project/new'>New Project</NavLink></li>
         <li><NavLink to='/'>LogOut</NavLink></li>
         <li><NavLink to='/' className="btn btn-floating pink lihten-1">DK</NavLink></li>
       </ul>

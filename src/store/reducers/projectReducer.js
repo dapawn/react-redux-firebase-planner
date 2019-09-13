0f1e1db10c2f4769156600028815e8e7025e155a
dapawn <dapawn@gmail.com>
2019-09-13 19:42:50 -0400

    Connected up Redux store.

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    {id: '1', title: 'Hear ye, Hear Ye, This is important', content: 'Now if I could just remember what I was going gto say. It was really important you know.'},
+    {id: '2', title: 'Hear ye, Hear Ye, Another important annoucement', content: 'Now if I could just remember what I was going gto say. It was really important you know.'},
+    {id: '3', title: 'Hear ye, Hear Ye', content: 'Okay maybe this is gettin old, but I just cant remember what I was going gto say. It was really important you know.'},
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;

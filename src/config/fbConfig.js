5cdeff9a87a269ebfc461a3aab97cb6ff0f5c5be
dapawn <dapawn@gmail.com>
2019-09-19 12:25:40 -0400

    Set up Firebase/Firestore.

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Init Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC5uBe0NE3Dx9q9wv_QdToDoaF5qJVdOak",
+  authDomain: "myplanner-3885c.firebaseapp.com",
+  databaseURL: "https://myplanner-3885c.firebaseio.com",
+  projectId: "myplanner-3885c",
+  storageBucket: "myplanner-3885c.appspot.com",
+  messagingSenderId: "551881922424",
+  appId: "1:551881922424:web:874b9116a076a3356c5a8d"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,18 @@
+
+
 export const createProject = (project) => {
-  return (dispatch, getState) => {
-    //make db call
-    dispatch({type: 'CREATE_PROJECT', project});
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    //make async db call
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'dapawn',
+      authorLastName: 'Kuru',
+      createdAt: new Date()
+    }).then( () => {
+      dispatch({type: 'CREATE_PROJECT', project});
+    }).catch( (err) => {
+      dispatch({type: 'CREATE_PROJECT_ERROR', err});
+    })
   }
 }

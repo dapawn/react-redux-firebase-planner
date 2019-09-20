50689610ea331ea6ae8c9f760619861fdf988b0f
dapawn <dapawn@gmail.com>
2019-09-20 14:38:11 -0400

    Added Firebase authentification to the App.

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authActions';
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
   return (
       <ul className="right">
         <li><NavLink to='/project/new'>New Project</NavLink></li>
-        <li><NavLink to='/'>LogOut</NavLink></li>
+        <li><a onClick={props.signOut}>LogOut</a></li>
         <li><NavLink to='/' className="btn btn-floating pink lihten-1">DK</NavLink></li>
       </ul>
   )
 }
 
-export default SignedInLinks;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut())
+  }
+}
+export default connect(null, mapDispatchToProps)(SignedInLinks);

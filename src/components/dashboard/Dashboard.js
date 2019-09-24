60c286a5513b29a0dd2fde315cf87069f182830d
dapawn <dapawn@gmail.com>
2019-09-23 22:00:39 -0400

    Made small update to Notification component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,10 +4,12 @@ import ProjectList from '../projects/ProjectList';
 import { connect } from 'react-redux'
 import { compose } from 'redux'
 import { firestoreConnect } from 'react-redux-firebase'
+import { Redirect } from 'react-router-dom'
 
 class Dashboard extends Component {
   render () {
     const { projects } = this.props;
+    if (!auth.uid) return <Redirect to='singin' />
   return (
     <div className="dashboard container">
       <div className="row">

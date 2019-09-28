f97279850aded4e9d8914583364b51be1947225e
dapawn <dapawn@gmail.com>
2019-09-27 21:07:12 -0400

    Added cloud funtions for notifications.

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import {compose} from 'redux';
 import {firestoreConnect} from 'react-redux-firebase';
 import { Redirect } from 'react-router-dom';
-import { moment } from 'moment';
+import moment from 'moment';
 
 const ProjectDetails = (props) => {
   const { project, auth } = props;
@@ -19,7 +19,7 @@ const ProjectDetails = (props) => {
           </div>
           <div className="card-action grey lighten-4 grey-text">
             <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
-            <div>{ moment(project.createdAt.toDate()).calendar() }</div>
+            <div>On {moment(project.createdAt.toDate()).calendar()} </div>
           </div>
         </div>
       </div>
